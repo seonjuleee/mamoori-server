ca3286ad2b9cc089c84bea906da9054c501129e9
seonjuleee <nlsj0820@naver.com>
2022-09-13 05:58:46 +0900

    init

---
+++ src/main/java/com/mamoori/mamooriback/MamooriBackApplication.java
@@ -0,0 +1,13 @@
+package com.mamoori.mamooriback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamooriBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamooriBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamoori/mamooriback/MamooriBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamoori.mamooriback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamooriBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
