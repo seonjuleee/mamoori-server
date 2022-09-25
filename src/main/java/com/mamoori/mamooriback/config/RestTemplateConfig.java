f8963c73d9e8757123bc159c7ec4b0f2843c2aae
Jioo Jung <jioome17@gmail.com>
2022-09-25 16:29:28 +0900

    Feat: kakao login

--- src/main/java/com/mamoori/mamooriback/config/RestTemplateConfig.java
+++ src/main/java/com/mamoori/mamooriback/config/RestTemplateConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 	@Bean
-	public RestTemplate restTemplate(){ // // 다른 서버의 API endpoint를 호출할 때 RestTemplate 사용
+	public RestTemplate restTemplate(){ // 다른 서버의 API endpoint를 호출할 때 RestTemplate 사용
 		RestTemplate restTemplate = new RestTemplate();
 
 		return restTemplate;
