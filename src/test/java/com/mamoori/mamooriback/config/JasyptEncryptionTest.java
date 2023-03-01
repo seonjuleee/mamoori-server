7bb0732d9b29d970cff02f9661ad8f8810e192df
seonjuleee <nlsj0820@naver.com>
2023-03-01 19:34:59 +0900

    #32 Feat: encrypt property file

---
+++ src/test/java/com/mamoori/mamooriback/config/JasyptEncryptionTest.java
@@ -0,0 +1,35 @@
+package com.mamoori.mamooriback.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JasyptEncryptionTest {
+    @Value("${jasypt.key}")
+    private String key;
+
+    @Test
+    void jasypt() {
+        String url = "url";
+        String username = "sa";
+        String password = "password";
+
+        printEncStr(url);
+        printEncStr(username);
+        printEncStr(password);
+    }
+
+    public void printEncStr(String str) {
+        System.out.println(str + " -> ENC(" + jasyptEncoding(str) + ")");
+    }
+
+    public String jasyptEncoding(String value) {
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+
+}
