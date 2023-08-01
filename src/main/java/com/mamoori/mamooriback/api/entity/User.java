96162c957d1893d36eec46a4ac6bdf504ec906b5
seonjuleee <nlsj0820@naver.com>
2023-08-02 00:09:35 +0900

    Chore: token, user의 id column 이름 변경

--- src/main/java/com/mamoori/mamooriback/api/entity/User.java
+++ src/main/java/com/mamoori/mamooriback/api/entity/User.java
@@ -18,7 +18,7 @@ public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "user_id")
     private Long userId;
 
     @Column(nullable = false)
