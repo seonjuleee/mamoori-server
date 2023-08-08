1833f62b58ca3eff356f095f6ae70544cdc56bc0
seonjuleee <nlsj0820@naver.com>
2023-08-08 22:10:09 +0900

    Chore: Filter request log 출력

--- src/main/java/com/mamoori/mamooriback/auth/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/mamoori/mamooriback/auth/filter/JwtAuthenticationProcessingFilter.java
@@ -53,6 +53,11 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
      * */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.debug("doFilterInternal called...");
+        log.debug("method : {}", request.getMethod());
+        log.debug("uri : {}", request.getRequestURI());
+        log.debug("host : {}", request.getRemoteHost());
+
         if (NO_CHECK_URL.contains(request.getRequestURI())) {
             filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
             return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
@@ -110,9 +115,9 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 //                // 로그인 종료
 //                CookieUtil.deleteCookie(request, response, jwtService.getRefreshHeader());
 //                // redirect login
-                response.sendRedirect("/login"); // TODO 수정하기
+//                response.sendRedirect("https://mamoori.life/login"); // TODO 수정하기
             }
-            filterChain.doFilter(request, response);
+//            filterChain.doFilter(request, response);
         }
     }
 }
