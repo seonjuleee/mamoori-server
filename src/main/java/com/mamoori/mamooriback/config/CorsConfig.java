1416c686cefdfa16deac97b4b0afd160e283188b
seonjuleee <nlsj0820@naver.com>
2023-08-13 14:48:16 +0900

    Fix: Client local test 시 CORS error 해결

--- src/main/java/com/mamoori/mamooriback/config/CorsConfig.java
+++ src/main/java/com/mamoori/mamooriback/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("https://mamoori.life", "https://api.mamoori.life"));
+        config.setAllowedOrigins(Arrays.asList("https://mamoori.life", "https://api.mamoori.life", "http://localhost:3000"));
         config.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         source.registerCorsConfiguration("/**",config);
