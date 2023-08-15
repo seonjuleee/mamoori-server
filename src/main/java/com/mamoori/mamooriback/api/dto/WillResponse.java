120270c9dc0599d88a3ab26f2c87eea36c3c41e7
seonjuleee <nlsj0820@naver.com>
2023-08-15 12:20:40 +0900

    Feat: Will Response 데이터 이름 변경

--- src/main/java/com/mamoori/mamooriback/api/dto/WillResponse.java
+++ src/main/java/com/mamoori/mamooriback/api/dto/WillResponse.java
@@ -9,28 +9,28 @@ import java.time.LocalDateTime;
 
 @Getter
 public class WillResponse {
-    private Long willId;
+    private Long id;
     private String title;
     private String content;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-    private LocalDateTime updateAt;
+    private LocalDateTime updatedAt;
 
     public WillResponse(Will will) {
-        this.willId = will.getWillId();
+        this.id = will.getWillId();
         this.title = will.getTitle();
         this.content = will.getContent();
-        this.createAt = will.getCreateAt();
-        this.updateAt = will.getUpdateAt();
+        this.createdAt = will.getCreateAt();
+        this.updatedAt = will.getUpdateAt();
     }
 
     @QueryProjection
-    public WillResponse(Long willId, String title, String content, LocalDateTime createAt, LocalDateTime updateAt) {
-        this.willId = willId;
+    public WillResponse(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
         this.title = title;
         this.content = content;
-        this.createAt = createAt;
-        this.updateAt = updateAt;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
