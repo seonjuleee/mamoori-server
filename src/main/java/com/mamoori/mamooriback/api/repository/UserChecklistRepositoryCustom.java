91f3b164721ab99a4a2657ff08c580a729ee95cb
seonjuleee <nlsj0820@naver.com>
2023-08-16 22:45:43 +0900

    Feat: 전체 Checklist 데이터 가공 로직을 Repository에서 Service로 변경

--- src/main/java/com/mamoori/mamooriback/api/repository/UserChecklistRepositoryCustom.java
+++ src/main/java/com/mamoori/mamooriback/api/repository/UserChecklistRepositoryCustom.java
@@ -1,14 +1,15 @@
 package com.mamoori.mamooriback.api.repository;
 
 import com.mamoori.mamooriback.api.dto.ChecklistDto;
-import com.mamoori.mamooriback.api.dto.ChecklistPageResponse;
+import com.mamoori.mamooriback.api.entity.UserChecklist;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UserChecklistRepositoryCustom {
-    ChecklistPageResponse getChecklistPage(String email, Pageable pageable);
+    Page<UserChecklist> getChecklistPage(String email, Pageable pageable);
     List<ChecklistDto> getChecklist(Long userChecklistId);
     Long getTotalTaskCount(Long userChecklistId);
     Long getCheckedTaskCount(Long userChecklistId);
